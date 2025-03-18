673f059a1309449e43e0f1c760e9b67e9e83a2bd
long hao <owm30912@mitac.com.tw>
2025-03-18 19:39:52 +0800

    add discovery service

---
+++ e-commerce/services/discovery/src/main/java/com/johnson/discovery/DiscoveryApplication.java
@@ -0,0 +1,14 @@
+package com.johnson.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+public class DiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryApplication.class, args);
+	}
+
+}
